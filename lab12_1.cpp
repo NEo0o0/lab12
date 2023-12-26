d8a698fb6bca5dab62c0fbe767b536ef990bd74c
NEo0o0 <nathdanai.nepal@gmail.com>
2023-12-26 15:19:52 +0700

    complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+long long int fibonacci(int x){
+    if(x==1){
+        return 1;}
+    if(x==0){
+        return 0;}
+    return fibonacci(x-1) + fibonacci(x-2);}
